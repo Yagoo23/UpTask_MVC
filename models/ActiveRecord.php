31c681f01b0139b9803f3610fde76aff3c7c74b5
Yagoo23 <yagocg7@gmail.com>
2024-02-05 13:46:16 +0100

    Finalizada creación de Proyectos. Inicio vistas del proyecto

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -68,6 +68,13 @@ class ActiveRecord {
         return array_shift( $resultado ) ;
     }
 
+     // Busca todos los registros que pertenecen a un id 
+     public static function belongsTo($columna, $valor) {
+        $query = "SELECT * FROM " . static::$tabla . " WHERE ${columna} = '${valor}'";
+        $resultado = self::consultarSQL($query);
+        return $resultado;
+    }
+
     // SQL para Consultas Avanzadas.
     public static function SQL($consulta) {
         $query = $consulta;
@@ -85,7 +92,7 @@ class ActiveRecord {
         $query .= join(', ', array_keys($atributos));
         $query .= " ) VALUES (' "; 
         $query .= join("', '", array_values($atributos));
-        $query .= " ') ";
+        $query .= "') ";
 
         // Resultado de la consulta
         $resultado = self::$db->query($query);
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,10 +1,23 @@
-<div class="dashboard">
-    <?php include_once __DIR__ . '/../templates/sidebar.php'; ?>
+<?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
-    <div class="principal">
-        <?php include_once __DIR__ . '/../templates/barra.php'; ?>
-        <div class="contenido">
-            <h2 class="nombre-pagina"><?php echo $titulo ?></h2>
-        </div>
-    </div>
-</div>
+<?php if (count($proyectos) === 0) { ?>
+
+    <p class="no-proyectos">No hay proyectos aún. <a href="/crear-proyecto">Comienza creando uno.</a> </p>
+
+<?php } else { ?>
+
+    <ul class="listado-proyectos">
+        <?php foreach ($proyectos as $proyecto) { ?>
+
+            <li class="proyecto">
+                <a href="/proyecto?id=<?php echo $proyecto->url ?>">
+                    <?php echo $proyecto->proyecto; ?>
+                </a>
+            </li>
+
+        <?php } ?>
+    </ul>
+
+<?php } ?>
+
+<?php include_once __DIR__ . '/footer-dashboard.php';
