e9397d91ce8a0ce38cce07fee52c92c0e4013512
Yagoo23 <yagocg7@gmail.com>
2024-02-19 12:39:50 +0100

    Creacion de ventana modal

---
+++ src/js/tareas.js
@@ -0,0 +1,43 @@
+(function () {
+    //Botón para mostrar el modal de agregar-tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+            <form class="formulario nueva-tarea">
+                <legend>Añade una nueva tarea</legend>
+                <div class="campo">
+                    <label>Tarea</label>
+                    <input type="text" name="tarea" placeholder="Añadir tarea al proyecto actual" id="tarea"/>
+                </div>
+                <div class="opciones">
+                    <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea" />
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form>
+        `;
+
+        setTimeout(() => {
+            const formulario = document.querySelector('.formulario');
+            formulario.classList.add('animar');
+        }, 0);
+
+        modal.addEventListener('click',function (e) {
+            e.preventDefault();
+
+            if (e.target.classList.contains('cerrar-modal')) {
+                const formulario = document.querySelector('.formulario');
+                formulario.classList.add('cerrar');
+                setTimeout(() => {
+                    modal.remove();
+                }, 500);
+            }
+        })
+
+        document.querySelector('body').appendChild(modal);
+
+    }
+})();
